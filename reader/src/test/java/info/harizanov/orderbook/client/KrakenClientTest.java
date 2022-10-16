2bac4fcb2f0becb8a65a15b45dc6fd4f16e567b0
Georgi Harizanov <gharizanov92@gmail.com>
2022-10-16 13:43:53 +0300

    wip

--- reader/src/test/java/info/harizanov/orderbook/client/KrakenClientTest.java
+++ reader/src/test/java/info/harizanov/orderbook/client/KrakenClientTest.java
@@ -1,19 +1,66 @@
 package info.harizanov.orderbook.client;
 
-import jakarta.websocket.ContainerProvider;
-import jakarta.websocket.DeploymentException;
-import jakarta.websocket.WebSocketContainer;
+import info.harizanov.orderbook.domain.message.encoder.KrakenSubscribeMessageEncoder;
+import jakarta.websocket.*;
+import org.glassfish.tyrus.core.TyrusEndpointWrapper;
+import org.glassfish.tyrus.core.coder.*;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Sinks;
 
 import java.io.IOException;
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.Flow;
 
 class KrakenClientTest {
 
     @Test
-    void connected() throws DeploymentException, IOException {
-        WebSocketContainer container =
+    void connected() throws DeploymentException, IOException, InterruptedException {
+        final WebSocketContainer container =
                 ContainerProvider.getWebSocketContainer();
-        container.connectToServer(KrakenClient.class, URI.create("wss://ws.kraken.com"));
+
+        final List<Class<? extends Decoder>> decoders = new ArrayList<>();
+        // decoders.addAll(PrimitiveDecoders.ALL);
+//        decoders.add(NoOpTextCoder.class);
+//        decoders.add(NoOpByteBufferCoder.class);
+//        decoders.add(NoOpByteArrayCoder.class);
+//        decoders.add(ReaderDecoder.class);
+//        decoders.add(InputStreamDecoder.class);
+
+        final ClientEndpointConfig clientEndpointConfig = ClientEndpointConfig.Builder.create()
+                .encoders(Collections.singletonList(KrakenSubscribeMessageEncoder.class))
+                .decoders(decoders)
+                .build();
+
+        final KrakenClient client = new KrakenClient();
+        container.connectToServer(client, clientEndpointConfig, URI.create("wss://ws.kraken.com"));
+
+         client.getProducer().asFlux().doOnNext(System.out::println).blockLast();
+//        client.subscribe(new Flow.Subscriber<String>() {
+//            @Override
+//            public void onSubscribe(Flow.Subscription subscription) {
+//
+//            }
+//
+//            @Override
+//            public void onNext(String item) {
+//                System.out.println(item);
+//            }
+//
+//            @Override
+//            public void onError(Throwable throwable) {
+//
+//            }
+//
+//            @Override
+//            public void onComplete() {
+//
+//            }
+//        });
+
+//        Thread.sleep(10000);
     }
 }
